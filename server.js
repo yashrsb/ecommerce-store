7fc640a9edc8ca11ca4943ed7d42a3c91f6ac89a
YashRaj-Git <yash.bisht@satschel.com>
2025-04-07 14:11:53 +0530

    add: cors; update package.json

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 const { seedProducts } = require("./data");
 
 app.use(express.json());
+
+app.use(cors());
 
 // Routes
 app.use("/cart", require("./routes/cart"));
